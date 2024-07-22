290a90f14f2034b060c650d280f7885c909a6c16
Nyamekesse <nyamekessesamuel@outlook.com>
2024-07-22 02:28:49 +0000

    created an extension class to register Infrastructure specific services in Program.cs

---
+++ Restaurants.Infrastructure/Extensions/ServiceCollectionExtension.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Restaurants.Infrastructure;
+
+public static class ServiceCollectionExtension
+{
+    public static void AddInfrastructure(
+        this IServiceCollection services,
+        IConfiguration configuration
+    )
+    {
+        var connectionString = configuration.GetConnectionString("DatabaseConnection");
+        services.AddDbContext<RestaurantDbContext>(options => options.UseNpgsql(connectionString));
+    }
+}

f64020ee8f1788c08d47e7419a88c4a87aa68888
Nyamekesse <nyamekessesamuel@outlook.com>
2024-07-22 17:27:38 +0000

    registered the all validations done by the fluent validations

--- Restaurants.Application/Extensions/ServiceCollectionExtensions.cs
+++ Restaurants.Application/Extensions/ServiceCollectionExtensions.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using FluentValidation;
+using FluentValidation.AspNetCore;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace Restaurants.Application;
 
@@ -6,7 +8,9 @@ public static class ServiceCollectionExtensions
 {
     public static void AddApplication(this IServiceCollection services)
     {
+        var applicationAssembly = typeof(ServiceCollectionExtensions).Assembly;
         services.AddScoped<IRestaurantsService, RestaurantService>();
-        services.AddAutoMapper(typeof(ServiceCollectionExtensions).Assembly);
+        services.AddAutoMapper(applicationAssembly);
+        services.AddValidatorsFromAssembly(applicationAssembly).AddFluentValidationAutoValidation();
     }
 }

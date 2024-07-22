ecc256f348dc59db0cbe95276841912f7dddf5be
Nyamekesse <nyamekessesamuel@outlook.com>
2024-07-22 12:02:11 +0000

    added a RestaurantService class and interface to communicate with the domain layer for processes

---
+++ Restaurants.Application/Restaurants/RestaurantService.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Extensions.Logging;
+using Restaurants.Domain;
+
+namespace Restaurants.Application;
+
+internal class RestaurantService(
+    IRestaurantRepository restaurantRepository,
+    ILogger<RestaurantService> logger
+) : IRestaurantsService
+{
+    public async Task<IEnumerable<Restaurant>> GetAllRestaurants()
+    {
+        logger.LogInformation("Getting all restaurants");
+        var restaurants = await restaurantRepository.GetAllAsync();
+        return restaurants;
+    }
+}

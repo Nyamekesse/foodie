62f4da5898264fe502a86c378720d6c086b69660
Nyamekesse <nyamekessesamuel@outlook.com>
2024-07-22 12:04:50 +0000

    created a restaurant repository class to handle all database interactions

---
+++ Restaurants.Infrastructure/Repositories/RestaurantRepository.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Restaurants.Domain;
+
+namespace Restaurants.Infrastructure;
+
+internal class RestaurantRepository(RestaurantDbContext _dbContext) : IRestaurantRepository
+{
+    public async Task<IEnumerable<Restaurant>> GetAllAsync()
+    {
+        var restaurants = await _dbContext.Restaurants.ToListAsync();
+        return restaurants;
+    }
+}

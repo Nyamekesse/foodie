d304ef382790ea31bb25d2326599023903f7f03d
Nyamekesse <nyamekessesamuel@outlook.com>
2024-07-22 13:07:19 +0000

    added a Dto to return specific fields

---
+++ Restaurants.Application/Restaurants/Dtos/RestaurantDto.cs
@@ -0,0 +1,33 @@
+﻿using Restaurants.Domain;
+
+namespace Restaurants.Application;
+
+public class RestaurantDto
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = default!;
+    public string Description { get; set; } = default!;
+    public string Category { get; set; } = default!;
+    public bool HasDelivery { get; set; }
+    public string? City { get; set; }
+    public string? Street { get; set; }
+    public string? PostalCode { get; set; }
+    public List<DishDto> Dishes { get; set; } = [];
+
+    public static RestaurantDto? FromRestaurant(Restaurant? restaurant)
+    {
+        if (restaurant is null)
+            return null;
+        return new RestaurantDto
+        {
+            Id = restaurant.Id,
+            Name = restaurant.Name,
+            Description = restaurant.Description,
+            Category = restaurant.Category,
+            HasDelivery = restaurant.HasDelivery,
+            City = restaurant.Address?.City,
+            Street = restaurant.Address?.Street,
+            PostalCode = restaurant.Address?.PostalCode
+        };
+    }
+}

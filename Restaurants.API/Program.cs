4812af0efcc5c626642f94643c6123792a71a471
Nyamekesse <nyamekessesamuel@outlook.com>
2024-07-22 03:23:19 +0000

    configured restaurant seeder function

--- Restaurants.API/Program.cs
+++ Restaurants.API/Program.cs
@@ -8,6 +8,9 @@ builder.Services.AddControllers();
 builder.Services.AddInfrastructure(builder.Configuration);
 
 var app = builder.Build();
+var scope = app.Services.CreateScope();
+var seeder = scope.ServiceProvider.GetRequiredService<IRestaurantSeeder>();
+await seeder.Seed();
 
 // Configure the HTTP request pipeline.
 

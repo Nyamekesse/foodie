e43c149615c09111b3b37176d5091ad936c98d66
Nyamekesse <nyamekessesamuel@outlook.com>
2024-07-22 12:00:43 +0000

    added a ServiceCollectionExtension class to register application layer specific services to scope

---
+++ Restaurants.Application/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace Restaurants.Application;
+
+public static class ServiceCollectionExtensions
+{
+    public static void AddApplication(this IServiceCollection services)
+    {
+        services.AddScoped<IRestaurantsService, RestaurantService>();
+    }
+}

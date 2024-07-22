4398f71540c39d1cfcaa209c3ace66b223599184
Nyamekesse <nyamekessesamuel@outlook.com>
2024-07-22 12:05:47 +0000

    registered add AddApplication service in Program.cs

--- Restaurants.API/Program.cs
+++ Restaurants.API/Program.cs
@@ -1,3 +1,4 @@
+using Restaurants.Application;
 using Restaurants.Infrastructure;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -6,6 +7,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddInfrastructure(builder.Configuration);
+builder.Services.AddApplication();
 
 var app = builder.Build();
 var scope = app.Services.CreateScope();

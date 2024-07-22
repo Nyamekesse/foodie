89aba62dbd61d2cfa25d8d7dcde670adfc6a71ec
Nyamekesse <nyamekessesamuel@outlook.com>
2024-07-22 14:21:51 +0000

    Added auto mapper to services, configured to map for all defined profiles

--- Restaurants.Application/Extensions/ServiceCollectionExtensions.cs
+++ Restaurants.Application/Extensions/ServiceCollectionExtensions.cs
@@ -7,5 +7,6 @@ public static class ServiceCollectionExtensions
     public static void AddApplication(this IServiceCollection services)
     {
         services.AddScoped<IRestaurantsService, RestaurantService>();
+        services.AddAutoMapper(typeof(ServiceCollectionExtensions).Assembly);
     }
 }

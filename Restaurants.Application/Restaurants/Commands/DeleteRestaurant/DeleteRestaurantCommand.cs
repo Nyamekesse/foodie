7ed411081ffb5c50ffa5d3775a0cbb5bce0b53f4
Nyamekesse <nyamekessesamuel@outlook.com>
2024-07-23 17:55:02 +0000

    added a command to delete a restaurant by id

---
+++ Restaurants.Application/Restaurants/Commands/DeleteRestaurant/DeleteRestaurantCommand.cs
@@ -0,0 +1,10 @@
+
+using MediatR;
+
+namespace Restaurants.Application.Restaurants.Commands.DeleteRestaurant
+{
+    public class DeleteRestaurantCommand(int id) : IRequest<bool>
+    {
+        public int Id { get; } = id;
+    }
+}
---
+++ Restaurants.Application/Restaurants/Commands/DeleteRestaurant/DeleteRestaurantCommandHandler.cs
@@ -0,0 +1,20 @@
+
+using MediatR;
+using Microsoft.Extensions.Logging;
+using Restaurants.Domain;
+
+namespace Restaurants.Application.Restaurants.Commands.DeleteRestaurant
+{
+    public class DeleteRestaurantCommandHandler(ILogger<DeleteRestaurantCommandHandler> logger, IRestaurantRepository restaurantRepository) : IRequestHandler<DeleteRestaurantCommand, bool>
+    {
+        public async Task<bool> Handle(DeleteRestaurantCommand request, CancellationToken cancellationToken)
+        {
+            logger.LogInformation($"Deleting restaurant with value {request.Id}");
+            var restaurant = await restaurantRepository.GetByIdAsync(request.Id);
+            if (restaurant is null) return false;
+
+            await restaurantRepository.Delete(restaurant);
+            return true;
+        }
+    }
+}

df73f6dd7f961f433b5a295019401f44990e4505
Nyamekesse <nyamekessesamuel@outlook.com>
2024-07-23 17:57:50 +0000

    added an interface implementation of delete

--- Restaurants.Domain/Respositories/IRestaurantRepository.cs
+++ Restaurants.Domain/Respositories/IRestaurantRepository.cs
@@ -5,4 +5,5 @@ public interface IRestaurantRepository
     Task<IEnumerable<Restaurant>> GetAllAsync();
     Task<Restaurant?> GetByIdAsync(int id);
     Task<int> Create(Restaurant restaurant);
+    Task Delete(Restaurant restaurant);
 }

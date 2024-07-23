44e168d83ea6e40db6be251ae541eeadfffc8bc6
Nyamekesse <nyamekessesamuel@outlook.com>
2024-07-23 05:10:59 +0000

    redistered mediatr commands and queries as a scoped service

--- Restaurants.Application/Extensions/ServiceCollectionExtensions.cs
+++ Restaurants.Application/Extensions/ServiceCollectionExtensions.cs
@@ -9,7 +9,9 @@ public static class ServiceCollectionExtensions
     public static void AddApplication(this IServiceCollection services)
     {
         var applicationAssembly = typeof(ServiceCollectionExtensions).Assembly;
-        services.AddScoped<IRestaurantsService, RestaurantService>();
+        services.AddMediatR(configuration =>
+            configuration.RegisterServicesFromAssembly(applicationAssembly)
+        );
         services.AddAutoMapper(applicationAssembly);
         services.AddValidatorsFromAssembly(applicationAssembly).AddFluentValidationAutoValidation();
     }

5e6e65d17fcc0ffffb149c62bcc5c56306b7f719
Nyamekesse <nyamekessesamuel@outlook.com>
2024-07-24 13:59:27 +0000

    added a custom exception middleware to intercept and handle exceptions thrown

---
+++ Restaurants.API/Middlewares/ErrorHandlingMiddleware.cs
@@ -0,0 +1,27 @@
+using Restaurants.Domain.Exceptions;
+
+namespace Restaurants.API.Middlewares
+{
+    public class ErrorHandlingMiddleware(ILogger<ErrorHandlingMiddleware> _logger) : IMiddleware
+    {
+        public async Task InvokeAsync(HttpContext context, RequestDelegate next)
+        {
+            try
+            {
+                await next.Invoke(context);
+            }
+            catch (NotFoundException notFound)
+            {
+                _logger.LogWarning(notFound.Message);
+                context.Response.StatusCode = 404;
+                await context.Response.WriteAsync(notFound.Message);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, ex.Message);
+                context.Response.StatusCode = 500;
+                await context.Response.WriteAsync("Something went wrong");
+            }
+        }
+    }
+}

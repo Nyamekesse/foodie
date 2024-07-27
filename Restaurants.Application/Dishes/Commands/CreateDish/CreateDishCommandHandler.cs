6860fc2a41ae8468e4ae661582b985a40c3b1716
Nyamekesse <nyamekessesamuel@outlook.com>
2024-07-27 23:49:17 +0000

    added a command for create dish, as well as the validator and command handler

---
+++ Restaurants.Application/Dishes/Commands/CreateDish/CreateDishCommandHandler.cs
@@ -0,0 +1,28 @@
+using AutoMapper;
+using MediatR;
+using Microsoft.Extensions.Logging;
+using Restaurants.Domain;
+using Restaurants.Domain.Exceptions;
+using Restaurants.Domain.Repositories;
+
+namespace Restaurants.Application.Dishes.Commands.CreateDish
+{
+    public class CreateDishCommandHandler(
+        ILogger _logger,
+        IRestaurantRepository _restaurantRepository,
+        IDishesRepository _dishesRepository,
+        IMapper _mapper
+    ) : IRequestHandler<CreateDishCommand>
+    {
+        public async Task Handle(CreateDishCommand request, CancellationToken cancellationToken)
+        {
+            _logger.LogInformation("Creating a new dish {@Dish}", request);
+            var restaurant = await _restaurantRepository.GetByIdAsync(request.RestaurantId);
+            if (restaurant is null)
+                throw new NotFoundException(nameof(Restaurant), request.RestaurantId.ToString());
+
+            var dish = _mapper.Map<Dish>(request);
+            await _dishesRepository.Create(dish);
+        }
+    }
+}

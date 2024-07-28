4fd989176ae81b5144e9d370385feb74e5a6d3ae
Nyamekesse <nyamekessesamuel@outlook.com>
2024-07-28 02:45:35 +0000

    added an interface method to delete all dishes

--- Restaurants.Domain/Respositories/IDishesRepository.cs
+++ Restaurants.Domain/Respositories/IDishesRepository.cs
@@ -3,5 +3,6 @@ namespace Restaurants.Domain.Repositories
     public interface IDishesRepository
     {
         Task<int> Create(Dish entity);
+        Task DeleteDishesForRestaurant(IEnumerable<Dish> entities);
     }
 }

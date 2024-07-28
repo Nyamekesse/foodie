1ca0c1f37494e6c883a69b64c79012b0e3b7a9cf
Nyamekesse <nyamekessesamuel@outlook.com>
2024-07-28 15:17:40 +0000

    added a migration to update a database with the identity manager tables

--- Restaurants.Infrastructure/Migrations/20240728150223_IdentityAdded.Designer.cs
+++ Restaurants.Infrastructure/Migrations/20240728150223_IdentityAdded.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using Restaurants.Infrastructure;
@@ -11,9 +12,11 @@ using Restaurants.Infrastructure;
 namespace Restaurants.Infrastructure.Migrations
 {
     [DbContext(typeof(RestaurantDbContext))]
-    partial class RestaurantDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240728150223_IdentityAdded")]
+    partial class IdentityAdded
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder

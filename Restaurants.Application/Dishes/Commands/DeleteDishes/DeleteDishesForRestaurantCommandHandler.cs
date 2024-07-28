2fb6ea5790e87a25b08a176a037142f91b3e775b
Nyamekesse <nyamekessesamuel@outlook.com>
2024-07-28 02:40:33 +0000

    implemented a command and handler to delete all dishes associated with a restaurant

---
+++ Restaurants.Application/Dishes/Commands/DeleteDishes/DeleteDishesForRestaurantCommandHandler.cs
@@ -0,0 +1,30 @@
+using MediatR;
+using Microsoft.Extensions.Logging;
+using Restaurants.Domain;
+using Restaurants.Domain.Exceptions;
+using Restaurants.Domain.Repositories;
+
+namespace Restaurants.Application.Dishes.Commands.DeleteDishes
+{
+    public class DeleteDishesForRestaurantCommandHandler(
+        ILogger<DeleteDishesForRestaurantCommandHandler> _logger,
+        IRestaurantRepository _restaurantRepository,
+        IDishesRepository _dishesRepository
+    ) : IRequestHandler<DeleteDishesForRestaurantCommand>
+    {
+        public async Task Handle(
+            DeleteDishesForRestaurantCommand request,
+            CancellationToken cancellationToken
+        )
+        {
+            _logger.LogWarning(
+                "Removing all dishes for restaurant {RestaurantId}",
+                request.RestaurantId
+            );
+            var restaurant = await _restaurantRepository.GetByIdAsync(request.RestaurantId);
+            if (restaurant is null)
+                throw new NotFoundException(nameof(Restaurant), request.RestaurantId.ToString());
+            await _dishesRepository.DeleteDishesForRestaurant(restaurant.Dishes);
+        }
+    }
+}

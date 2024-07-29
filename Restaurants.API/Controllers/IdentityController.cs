df038b5fb243670a8e03bb677d42758e675a70a3
Nyamekesse <nyamekessesamuel@outlook.com>
2024-07-29 21:42:20 +0000

    added an end point to assign a role to a user

--- Restaurants.API/Controllers/IdentityController.cs
+++ Restaurants.API/Controllers/IdentityController.cs
@@ -1,7 +1,9 @@
 using MediatR;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
-using Restaurants.Application.Users.Commands;
+using Restaurants.Application.Users.Commands.AssignUserRoles;
+using Restaurants.Application.Users.Commands.UpdateUserDetail;
+using Restaurants.Domain.Constants;
 
 namespace Restaurants.API.Controllers
 {
@@ -12,6 +14,14 @@ namespace Restaurants.API.Controllers
         [HttpPatch("user-details")]
         [Authorize]
         public async Task<IActionResult> UpdateUserDetails(UpdateUserDetailsCommand command)
+        {
+            await mediator.Send(command);
+            return NoContent();
+        }
+
+        [HttpPost("user-role")]
+        [Authorize(Roles = UserRoles.Admin)]
+        public async Task<IActionResult> AssignUserRole(AssignUserRoleCommand command)
         {
             await mediator.Send(command);
             return NoContent();

cb600efcd6a88bcccae20e0fde15b35540d79d1c
Nyamekesse <nyamekessesamuel@outlook.com>
2024-07-30 17:27:16 +0000

    updated database, added relation between restaurant and owner via migration

--- Restaurants.Infrastructure/Migrations/20240730165721_RestaurantOwnerAdded.Designer.cs
+++ Restaurants.Infrastructure/Migrations/20240730165721_RestaurantOwnerAdded.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using Restaurants.Infrastructure;
@@ -11,9 +12,11 @@ using Restaurants.Infrastructure;
 namespace Restaurants.Infrastructure.Migrations
 {
     [DbContext(typeof(RestaurantDbContext))]
-    partial class RestaurantDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240730165721_RestaurantOwnerAdded")]
+    partial class RestaurantOwnerAdded
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder

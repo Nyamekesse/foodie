268c43f9295f4e370fc686b20bbffb0a9d071e42
Nyamekesse <nyamekessesamuel@outlook.com>
2024-07-30 22:54:38 +0000

    added a new policy claim constant CreatedAtLeast2Restaurants

--- Restaurants.Infrastructure/Authorization/Constants.cs
+++ Restaurants.Infrastructure/Authorization/Constants.cs
@@ -4,6 +4,7 @@ namespace Restaurants.Infrastructure.Authorization
     {
         public const string HasNationality = "HasNationality";
         public const string AtLeast20YearsOld = "AtLeast20YearsOld";
+        public const string CreatedAtLeast2Restaurants = "CreatedAtLeast2Restaurants";
     }
 
     public static class AppClaimTypes

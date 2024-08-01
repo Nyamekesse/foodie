f0af461f8ecd294f21d3ebe5e0a9ad3f259fa9cc
Nyamekesse <nyamekessesamuel@outlook.com>
2024-08-01 15:59:06 +0000

    refactored query and handler to pass the pagesize and number for pagination

--- Restaurants.Application/Restaurants/Queries/GetAllRestaurants/GetAllRestaurantQuery.cs
+++ Restaurants.Application/Restaurants/Queries/GetAllRestaurants/GetAllRestaurantQuery.cs
@@ -1,6 +1,12 @@
 using MediatR;
+using Restaurants.Application.Common;
 
 namespace Restaurants.Application.Restaurants.Queries.GetAllRestaurants
 {
-    public class GetAllRestaurantQuery : IRequest<IEnumerable<RestaurantDto>> { }
+    public class GetAllRestaurantQuery : IRequest<PagedResult<RestaurantDto>>
+    {
+        public string? SearchPhrase { get; set; }
+        public int PageNumber { get; set; }
+        public int PageSize { get; set; }
+    }
 }
--- Restaurants.Application/Restaurants/Queries/GetAllRestaurants/GetAllRestaurantsHandler.cs
+++ Restaurants.Application/Restaurants/Queries/GetAllRestaurants/GetAllRestaurantsHandler.cs
@@ -1,6 +1,7 @@
 using AutoMapper;
 using MediatR;
 using Microsoft.Extensions.Logging;
+using Restaurants.Application.Common;
 using Restaurants.Domain;
 
 namespace Restaurants.Application.Restaurants.Queries.GetAllRestaurants
@@ -9,17 +10,27 @@ namespace Restaurants.Application.Restaurants.Queries.GetAllRestaurants
         IRestaurantRepository restaurantRepository,
         ILogger<GetAllRestaurantsHandler> logger,
         IMapper mapper
-    ) : IRequestHandler<GetAllRestaurantQuery, IEnumerable<RestaurantDto>>
+    ) : IRequestHandler<GetAllRestaurantQuery, PagedResult<RestaurantDto>>
     {
-        public async Task<IEnumerable<RestaurantDto>> Handle(
+        public async Task<PagedResult<RestaurantDto>> Handle(
             GetAllRestaurantQuery request,
             CancellationToken cancellationToken
         )
         {
             logger.LogInformation("Getting all restaurants");
-            var restaurants = await restaurantRepository.GetAllAsync();
-            var restaurantDto = mapper.Map<IEnumerable<RestaurantDto>>(restaurants);
-            return restaurantDto;
+            var (restaurants, totalCount) = await restaurantRepository.GetAllMatchingAsync(
+                request.SearchPhrase,
+                request.PageSize,
+                request.PageNumber
+            );
+            var restaurantDtos = mapper.Map<IEnumerable<RestaurantDto>>(restaurants);
+            var result = new PagedResult<RestaurantDto>(
+                restaurantDtos,
+                totalCount,
+                request.PageSize,
+                request.PageNumber
+            );
+            return result;
         }
     }
 }

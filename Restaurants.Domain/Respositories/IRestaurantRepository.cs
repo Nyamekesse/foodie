70badf026615aa9fca6098651250c983b1ce704e
Nyamekesse <nyamekessesamuel@outlook.com>
2024-08-01 16:05:02 +0000

    added a method GetAllMatchingAsync

--- Restaurants.Domain/Respositories/IRestaurantRepository.cs
+++ Restaurants.Domain/Respositories/IRestaurantRepository.cs
@@ -3,6 +3,11 @@
 public interface IRestaurantRepository
 {
     Task<IEnumerable<Restaurant>> GetAllAsync();
+    Task<(IEnumerable<Restaurant>, int)> GetAllMatchingAsync(
+        string? searchPhrase,
+        int pageSize,
+        int pageNumber
+    );
     Task<Restaurant?> GetByIdAsync(int id);
     Task<int> Create(Restaurant restaurant);
     Task Delete(Restaurant restaurant);

3dd8a034988c1dca70fc7c20f0e2329559fe3dcd
Nyamekesse <nyamekessesamuel@outlook.com>
2024-08-01 17:30:52 +0000

    refactored code

--- Restaurants.Application/Restaurants/Dtos/RestaurantDto.cs
+++ Restaurants.Application/Restaurants/Dtos/RestaurantDto.cs
@@ -13,22 +13,4 @@ public class RestaurantDto
     public string? Street { get; set; }
     public string? PostalCode { get; set; }
     public List<DishDto> Dishes { get; set; } = [];
-
-    // public static RestaurantDto? FromRestaurant(Restaurant? restaurant)
-    // {
-    //     if (restaurant is null)
-    //         return null;
-    //     return new RestaurantDto
-    //     {
-    //         Id = restaurant.Id,
-    //         Name = restaurant.Name,
-    //         Description = restaurant.Description,
-    //         Category = restaurant.Category,
-    //         HasDelivery = restaurant.HasDelivery,
-    //         City = restaurant.Address?.City,
-    //         Street = restaurant.Address?.Street,
-    //         PostalCode = restaurant.Address?.PostalCode,
-    //         Dishes = restaurant.Dishes.Select(DishDto.FromEntity).ToList(),
-    //     };
-    // }
 }

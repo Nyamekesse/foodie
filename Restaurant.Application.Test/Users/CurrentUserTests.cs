89c3ddd4d745748876fe3c903808fe964fc517d3
Nyamekesse <nyamekessesamuel@outlook.com>
2024-08-01 23:36:03 +0000

    created test cases for currentUser

---
+++ Restaurant.Application.Test/Users/CurrentUserTests.cs
@@ -0,0 +1,63 @@
+using FluentAssertions;
+using Restaurants.Application.Users;
+using Restaurants.Domain.Constants;
+using Xunit;
+
+namespace Restaurant.Application.Test.Users
+{
+    public class CurrentUserTests
+    {
+        [Theory()]
+        [InlineData(UserRoles.Admin)]
+        [InlineData(UserRoles.User)]
+        public void IsInRole_WithMatchingRole_ShouldReturnTrue(string roleName)
+        {
+            // arrange
+            var currentUser = new CurrentUser(
+                "1",
+                "test@test.com",
+                [UserRoles.Admin, UserRoles.User],
+                null,
+                null
+            );
+            // act
+            var isInRole = currentUser.IsInRole(roleName);
+            // assert
+            isInRole.Should().BeTrue();
+        }
+
+        [Fact]
+        public void IsInRole_WithNoMatchingRole_ShouldReturnFalse()
+        {
+            // arrange
+            var currentUser = new CurrentUser(
+                "1",
+                "test@test.com",
+                [UserRoles.Admin, UserRoles.User],
+                null,
+                null
+            );
+            // act
+            var isInRole = currentUser.IsInRole(UserRoles.Owner);
+            // assert
+            isInRole.Should().BeFalse();
+        }
+
+        [Fact]
+        public void IsInRole_WithNoMatchingRoleCase_ShouldReturnFalse()
+        {
+            // arrange
+            var currentUser = new CurrentUser(
+                "1",
+                "test@test.com",
+                [UserRoles.Admin, UserRoles.User],
+                null,
+                null
+            );
+            // act
+            var isInRole = currentUser.IsInRole(UserRoles.Admin.ToLower());
+            // assert
+            isInRole.Should().BeFalse();
+        }
+    }
+}

bff1cd81eb23e9ea07831f1ec709cf30301a3d49
Nyamekesse <nyamekessesamuel@outlook.com>
2024-08-05 02:34:37 +0000

    wrapped the application startup in a try catch block to handle when application is not able to start and also log the log details

--- src/Restaurants.API/Program.cs
+++ src/Restaurants.API/Program.cs
@@ -6,41 +6,52 @@ using Restaurants.Domain.Entities;
 using Restaurants.Infrastructure;
 using Serilog;
 
-var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
-
-
-builder.AddPresentation();
-builder.Services.AddApplication();
-builder.Services.AddInfrastructure(builder.Configuration);
-
-var app = builder.Build();
-var scope = app.Services.CreateScope();
-var seeder = scope.ServiceProvider.GetRequiredService<IRestaurantSeeder>();
-await seeder.Seed();
-
-app.UseMiddleware<ErrorHandlingMiddleware>();
-app.UseMiddleware<RequestsTimeLoggingMiddleware>();
-
-app.UseSerilogRequestLogging();
-
-if (app.Environment.IsDevelopment())
+try
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    var builder = WebApplication.CreateBuilder(args);
+
+    // Add services to the container.
+
+
+    builder.AddPresentation();
+    builder.Services.AddApplication();
+    builder.Services.AddInfrastructure(builder.Configuration);
+
+    var app = builder.Build();
+    var scope = app.Services.CreateScope();
+    var seeder = scope.ServiceProvider.GetRequiredService<IRestaurantSeeder>();
+    await seeder.Seed();
+
+    app.UseMiddleware<ErrorHandlingMiddleware>();
+    app.UseMiddleware<RequestsTimeLoggingMiddleware>();
+
+    app.UseSerilogRequestLogging();
+
+    if (app.Environment.IsDevelopment())
+    {
+        app.UseSwagger();
+        app.UseSwaggerUI();
+    }
+
+    // Configure the HTTP request pipeline.
+
+    app.UseHttpsRedirection();
+
+    app.MapGroup("api/identity").WithTags("Identity").MapIdentityApi<User>();
+
+    app.UseAuthorization();
+
+    app.MapControllers();
+
+    app.Run();
 }
-
-// Configure the HTTP request pipeline.
-
-app.UseHttpsRedirection();
-
-app.MapGroup("api/identity").WithTags("Identity").MapIdentityApi<User>();
-
-app.UseAuthorization();
-
-app.MapControllers();
-
-app.Run();
+catch (Exception ex)
+{
+    Log.Fatal(ex, "Application startup failed");
+}
+finally
+{
+    Log.CloseAndFlush();
+}
 
 public partial class Program { }
